b112dd950376e51004a0bede551ed5548c2eb324
AyariAhmed <ayari17@hotmail.fr>
2022-05-31 01:01:41 +0100

    set eslint to only-warn

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,7 @@ module.exports = {
     project: 'tsconfig.json',
     sourceType: 'module',
   },
-  plugins: ['@typescript-eslint/eslint-plugin'],
+  plugins: ['@typescript-eslint/eslint-plugin', 'only-warn'],
   extends: [
     'plugin:@typescript-eslint/recommended',
     'prettier/@typescript-eslint',
